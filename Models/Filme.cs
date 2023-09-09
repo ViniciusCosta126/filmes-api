72e30bba0e304934a014d42d6703f181cdf1b0e3
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2023-09-09 14:53:33 -0300

    Configuracoes iniciais do projeto com entity

---
+++ Models/Filme.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O Titulo do filme é obrigatorio")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O Genero do filme é obrigatorio")]
+    [MaxLength(50, ErrorMessage = "O tamanho do genero nao pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "O campo duração é obrigatorio")]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
